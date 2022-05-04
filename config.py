9f8322b55a94fc6654235775cc6252add49ede05
nealwaga <waganeal@gmail.com>
2022-05-04 15:55:51 +0300

    renamed init file

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
-    # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
-    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
-    CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey=3a10d7cd5abf4e089804482dab06b7bd'
+    ALL_NEWS_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=3a10d7cd5abf4e089804482dab06b7bd'
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
--- manage.py
+++ manage.py
@@ -1,19 +1,11 @@
 from app import create_app
-from flask_script import Manager,Server
+from flask_script import Manager, Server
 
-#Creating app instance
+#Creating the app instance
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server',Server)
-
-#@manager.command
-#def test():
-#    """Run the unit tests."""
-#    import unittest
-#    tests = unittest.TestLoader().discover('tests')
-#    unittest.TextTestRunner(verbosity=2).run(tests)
-
+manager.add_command('server', Server)
 
 if __name__ == '__main__':
     manager.run()

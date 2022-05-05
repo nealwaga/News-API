c0365b053b45a2189416d03a24cd8a60e9482afd
nealwaga <waganeal@gmail.com>
2022-05-05 11:15:20 +0300

    Some message

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
-app = Flask (__name__)
+app=Flask(__name__)
 
-@app.route('/')
-def home ():
-    return "<h1> Hello </h1>"
+@app.route("/")
+def intro():
+    #home.html
+    return "<h1>This is my deployed application</h1>"
 
-if __name__ == '__main__':
-    app.run()    
+
+if __name__=="__main__":    
+    app.run(debug=True)
